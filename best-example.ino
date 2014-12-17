b0968a5f2bb256c7f6668735b39e39badb65febb
Jeremy Ellis <jellis@hpssapps.com>
2014-12-16 23:34:56 -0800

    Update best-example.ino

--- best-example.ino
+++ best-example.ino
@@ -1,25 +1,56 @@
-int temp = 0;
+
+
+int analogvalue = 0;
+double tempC = 0.0;
+char *message = "My name is spark";
+
+
+
+
+
 
 void setup() {
-  pinMode(D7, OUTPUT);
-  pinMode(A0, INPUT);
+  pinMode(D7, OUTPUT);  //set D7 to set the LED
   Spark.function("d7-on", d7On);
   Spark.function("d7-off", d7Off);
   
   
   RGB.control(true);
   RGB.color(0, 255, 255);  //cyan
-  RGB.brightness(4);    // very low light
+  RGB.brightness(1);    // 1=very low light, 255 = max
+  
+  
+  pinMode(A0, INPUT);   // set A0 for reading data
+  Spark.variable("a0-read", &analogvalue, INT);
+  Spark.variable("a0-temp", &tempC, DOUBLE);
+  Spark.variable("mess", message, STRING);
+
   
   
 }
 
+
+
+
+
+
+
 void loop() {
-    delay(4000);
+    
+    double voltage = 0.0;
+    
+    analogvalue = analogRead(A0);
+    voltage = (analogvalue * 3.3) / 4095;  // convert reading to voltage
+    tempC  = (voltage - 0.5) * 100;     //Convert the reading into degree celcius
+    
+    
+    
+    delay(2000);    
     RGB.color(0, 255, 255);
-    delay(4000);
+    delay(2000);
     RGB.color(0, 0, 0);
-   // nothing here
+    
+
 }
 
 int d7On(String args) {
@@ -29,5 +60,5 @@ int d7On(String args) {
 
 int d7Off(String args) {
   digitalWrite(D7, LOW);
-  return 1;
+  return 0;
 }
